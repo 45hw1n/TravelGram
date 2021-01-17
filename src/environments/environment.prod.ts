394422da9453f3705b1d85360ca1ad5c1b2ee176
45hw1n <ashwin.sundar.04@gmail.com>
2021-01-17 10:50:00 +0530

    TravelGram

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBpV66S9KX3XyyKNNstzeMV396E8DJtnS4",
+    authDomain: "travelgram-a27f0.firebaseapp.com",
+    databaseURL: "https://travelgram-a27f0.firebaseio.com",
+    projectId: "travelgram-a27f0",
+    storageBucket: "travelgram-a27f0.appspot.com",
+    messagingSenderId: "503029702567",
+    appId: "1:503029702567:web:bb573096cf1c959948a082",
+    measurementId: "G-BVWXYWSZZG"
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBpV66S9KX3XyyKNNstzeMV396E8DJtnS4",
+    authDomain: "travelgram-a27f0.firebaseapp.com",
+    databaseURL: "https://travelgram-a27f0.firebaseio.com",
+    projectId: "travelgram-a27f0",
+    storageBucket: "travelgram-a27f0.appspot.com",
+    messagingSenderId: "503029702567",
+    appId: "1:503029702567:web:bb573096cf1c959948a082",
+    measurementId: "G-BVWXYWSZZG"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
